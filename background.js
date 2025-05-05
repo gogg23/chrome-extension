283b2937af2ae891f65eb2a8b642c032fe3ea083
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2025-05-05 14:38:59 +0300

    commit

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
 //chrome
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^https:/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: { tabId},
-            files: ['./content.js']
-        }).then(() => {
-            console.log('We have injected the content script!'                
-            }).catch((err) => {
-                console.log(err, "error in background.js line 10");
-            });
-            }
+  if (changeInfo.status === 'complete' && /^https:/.test(tab.url)) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
+        files: ['./content.js'],
+      })
+      .then(() => {
+        console.log('We have injected the content script!');
+      })
+      .catch((err) => {
+        console.log(err, 'error in background.js line 10');
+      });
+  }
 });
